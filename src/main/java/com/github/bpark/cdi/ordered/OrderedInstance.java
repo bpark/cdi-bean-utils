7e300833e2007ae8d03ba13f8fd03cee5c69692e
bpark <burt.parkers@googlemail.com>
2017-12-15 13:20:04 +0100

    removed Instance implementation to prevent ambiguous dependencies error

--- src/main/java/com/github/bpark/cdi/ordered/OrderedInstance.java
+++ src/main/java/com/github/bpark/cdi/ordered/OrderedInstance.java
@@ -20,54 +20,47 @@ package com.github.bpark.cdi.ordered;
 
 import javax.enterprise.inject.Instance;
 import javax.enterprise.util.TypeLiteral;
+import javax.inject.Provider;
 import java.lang.annotation.Annotation;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.Iterator;
 import java.util.List;
 
-public class OrderedInstance<T> implements Instance<T> {
+public class OrderedInstance<T> implements Iterable<T>, Provider<T> {
 
     private Instance<T> instance;
     private Iterator<T> iterator;
 
-    @Override
     public Iterator<T> iterator() {
         return iterator;
     }
 
 
-    @Override
     public Instance<T> select(Annotation... annotations) {
         return instance.select(annotations);
     }
 
-    @Override
     public <U extends T> Instance<U> select(Class<U> aClass, Annotation... annotations) {
         return instance.select(aClass, annotations);
     }
 
-    @Override
     public <U extends T> Instance<U> select(TypeLiteral<U> typeLiteral, Annotation... annotations) {
         return instance.select(typeLiteral, annotations);
     }
 
-    @Override
     public boolean isUnsatisfied() {
         return instance.isUnsatisfied();
     }
 
-    @Override
     public boolean isAmbiguous() {
         return instance.isAmbiguous();
     }
 
-    @Override
     public void destroy(T t) {
         instance.destroy(t);
     }
 
-    @Override
     public T get() {
         return instance.get();
     }
